c9f42699716cca7b9285ab019ed83ed5f86a13a9
Jayla Kaita <jaylakai@hawaii.edu>
2019-10-17 00:16:04 -1000

    done

---
+++ Lab9/ex1.js
@@ -0,0 +1,20 @@
+> date = '3/22/1999'
+'3/22/1999'
+> step1 = 99;
+99
+> step2 = step1/4
+24.75
+> step2 = parseInt(step1/4);
+24
+> step3 = step2 + step1;
+123
+> step4 = 6;
+6
+> step6 = step4 + step3;
+129
+> step7 = step6 + 22
+151
+> step8 = step7/7
+21.571428571428573
+> step8 = step7%7
+4
