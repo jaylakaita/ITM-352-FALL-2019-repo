944742d449749baa312aa30147bfc15d310ed1e4
Jayla Kaita <jaylakai@hawaii.edu>
2019-11-24 16:08:13 -1000

    1

--- Assignment2Edit/server.js
+++ Assignment2Edit/server.js
@@ -3,7 +3,7 @@ const querystring = require('querystring');
 
 var express = require('express');
 var myParser = require("body-parser");
-var products = require("./public/product.js.js");
+var products = require("./public/product.js");
 
 var app = express();
 app.all('*', function (request, response, next) {
--- Assignment2Edit/validate.js
+++ Assignment2Edit/validate.js
@@ -1,69 +1,80 @@
-var validate = require("validate.js");
-var constraints = {
-    username: {
-      presence: true,
-      exclusion: {
-        within: ["nicklas"],
-        message: "'%{value}' is not allowed"
-      }
-    },
-    password: {
-      presence: true,
-      length: {
-        minimum: 6,
-        message: "must be at least 6 characters"
-      }
-    }
-  };
-  
-  validate({}, constraints, {format: "flat"});
-  // => ["Username can't be blank", "Password can't be blank"]
-  
-  validate({username: "nicklas", password: "bad"}, constraints, {format: "detailed"});
-  // => [
-  //   {
-  //     "attribute": "username",
-  //     "value": "nicklas",
-  //     "validator": "exclusion",
-  //     "globalOptions": {
-  //       "format": "detailed"
-  //     },
-  //     "attributes": {
-  //       "username": "nicklas",
-  //       "password": "bad"
-  //     },
-  //     "options": {
-  //       "within": [
-  //         "nicklas"
-  //       ],
-  //       "message": "'%{value}' is not allowed"
-  //     },
-  //     "error": "Username 'nicklas' is not allowed"
-  //   },
-  //   {
-  //     "attribute": "password",
-  //     "value": "bad",
-  //     "validator": "length",
-  //     "globalOptions": {
-  //       "format": "detailed"
-  //     },
-  //     "attributes": {
-  //       "username": "nicklas",
-  //       "password": "bad"
-  //     },
-  //     "options": {
-  //       "minimum": 6,
-  //       "message": "must be at least 6 characters"
-  //     },
-  //     "error": "Password must be at least 6 characters"
-  //   }
-  // ]
-  
-  validate.formatters.custom = function(errors) {
-    return errors.map(function(error) {
-      return error.validator;
-    });
-  };
-  
-  validate({username: "nicklas", password: "bad"}, constraints, {format: "custom"});
-  // => ["exclusion", "length"];
+function formValidation()
+{
+var uid = document.registration.userid;
+var passid = document.registration.passid;
+var uname = document.registration.username;
+var uemail = document.registration.email; 
+if(userid_validation(uid,5,12))
+{
+if(passid_validation(passid,7,12))
+{
+if(allLetter(uname))
+{
+if(ValidateEmail(uemail))
+{
+}
+} 
+}
+} 
+return false;
+}
+
+function userid_validation(uid,mx,my)
+{
+var uid_len = uid.value.length;
+if (uid_len == 0 || uid_len >= my || uid_len < mx)
+{
+alert("User Id should not be empty / length be between "+mx+" to "+my);
+uid.focus();
+return false;
+}
+return true;
+}
+
+function passid_validation(passid,mx,my)
+{
+var passid_len = passid.value.length;
+if (passid_len == 0 ||passid_len >= my || passid_len < mx)
+{
+alert("Password should not be empty / length be between "+mx+" to "+my);
+passid.focus();
+return false;
+}
+return true;
+}
+
+function allLetter(uname)
+{ 
+var letters = /^[A-Za-z]+$/;
+if(uname.value.match(letters))
+{
+return true;
+}
+else
+{
+alert('Username must have alphabet characters only');
+uname.focus();
+return false;
+}
+}
+
+function ValidateEmail(uemail)
+{
+var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+if(uemail.value.match(mailformat))
+{
+return true;
+}
+else
+{
+alert("You have entered an invalid email address!");
+uemail.focus();
+return false;
+}
+}
+
+{
+alert('Account Successfully Registered');
+window.location.reload()
+return true;
+}
