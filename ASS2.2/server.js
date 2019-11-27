4369d8b375339542b5fa3e2cf70de181bf01a26a
Jayla Kaita <jaylakai@hawaii.edu>
2019-11-27 13:43:42 -1000

    k

--- ASS2.2/server.js
+++ ASS2.2/server.js
@@ -7,14 +7,6 @@ var products = require("./public/product.js"); //uses flower products listed in 
 var filename = 'user_data.json' //Defines the user_data.json array as an object
 var app = express(); //Executes Express
 
-
-
-
-app.all('*', function (request, response, next) {
-   console.log(request.method + ' to ' + request.path); //respond to HTTP request by sending type of request and the path of request
-   next(); //calls the middleware function
-});
-
 app.use(myParser.urlencoded({ extended: true }));
 //intercept purchase submission form, if good give an invoice, otherwise send back to order page
 app.get("/process_page", function (request, response) {
@@ -111,27 +103,24 @@ app.get("/register", function (request, response) {
    // Give a simple register form
    
    str = `
-<body>
-<form action="/action_page.php" method="POST">
-<input type="text" name="username" size="40" pattern=".[a-z0-9]{4,10}" required title="Either 4-10 Characters & only numbers/letters" placeholder="enter username" ><br />
-<input type="email" name="email" size="40" placeholder="enter email" pattern="[a-z0-9._]+@[a-z0-9]+\.[a-z]{3,}$" required title="Error!! Make sure your email contains the following... 1. @ sign 2. Three letters in domain name 3. Only numbers/characters and _ & . may be used. "><br />
-<input type="password" name="repeat_password" size="40" pattern=".{6,}" required title="6 characters minimum" placeholder="enter password again"><br />
-<input type="password" name="repeat_password" size="40" pattern=".{6,}" required title="6 characters minimum" placeholder="enter password again"><br />
-<input type="submit" value="Submit" id="submit">
-</form>
-</body>
+   <body>
+      <form action="/action_page.php" method="POST">
+         <input type="text" name="username" size="40" pattern=".[a-z0-9]{4,10}" required title="Either 4-10 Characters & only numbers/letters" placeholder="enter username" ><br />
+         <input type="email" name="email" size="40" placeholder="enter email" pattern="[a-z0-9._]+@[a-z0-9]+\.[a-z]{3,}$" required title="Error!! Make sure your email contains the following... 1. @ sign 2. Three letters in domain name 3. Only numbers/characters and _ & . may be used. "><br />
+         <input type="password" name="repeat_password" size="40" pattern=".{6,}" required title="6 characters minimum" placeholder="enter password again"><br />
+         <input type="password" name="repeat_password" size="40" pattern=".{6,}" required title="6 characters minimum" placeholder="enter password again"><br />
+         <input type="submit" value="Submit" id="submit">
+      </form>
+   </body>
    `;
    response.send(str);
 });
 
 app.post("/register", function (request, response) {
    // process a simple register form
-
    //Validate
-
    //Save new user to file name (users_reg_data)
    username = request.body.username;
-   
    //Checks to see if username already exists
    errors = [];
  
@@ -160,9 +149,10 @@ if (errors.length == 0){
 });
 
 
-
-
-
+app.all('*', function (request, response, next) {
+   console.log(request.method + ' to ' + request.path); //respond to HTTP request by sending type of request and the path of request
+   next(); //calls the middleware function
+});
 
 app.use(express.static('./public'));
 app.listen(8080, () => console.log(`listening on port 8080`));
--- Lab14/A2.js
+++ Lab14/A2.js
@@ -1,47 +1,39 @@
 //processing a login
+var fs = require('fs');
 var express = require('express');
 var app = express();
 var myParser = require("body-parser");
+var qs = require('querystring');
 
 app.use(myParser.urlencoded({ extended: true }));
-
-fs = require('fs');
-
 var filename = 'user_data.json';
 
 if(fs.existsSync(filename)) {
-//im going to check if the file exists
     stats = fs.statSync(filename);
-
+    
     console.log(filename + ' has ' + stats.size + 'characters');
-    //stat size give info in the file
+    
     data = fs.readFileSync(filename, 'utf-8') 
-
     users_reg_data = JSON.parse(data);
-    //converts string into object
-/*
-    username = 'newuser';
-    users_reg_data[username] = {};
-    users_reg_data[username].password = 'newpass';
-    users_reg_data[username].email = 'newuser@user.com';
-    //successfully adds another user to user_reg_data
-    fs.writeFileSync(filename, JSON.stringify(users_reg_data));
-    //take user reg data and convert to string and srite into file
-    //convert to json
-   */ 
     console.log(users_reg_data);
-    //console.log(users_reg_data.["itm352"]);
-    //return's that object(users)info
-    //console.log(users_reg_data.["itm352"]["password"]);
-    //find the password of itm352
-    //console.log(users_reg_data.["itm352"]= {});
-
 } 
 //if does not exist say it does not exist
 else {
     console.log(filename + 'does not exist!')
 }
+var user_product_quantities = {};
 
+app.get("/purchase", function (request, response) {
+    //quantity data in QS
+    user_product_quantities = request.query;
+    //store in user product quantities
+    //do the validation etc
+    //if not valid, go back to products display
+    //otherwise go to login
+    response.redirect('login');
+    //for this server the end of the path is login
+    //does a get to login
+});
 
 app.get("/login", function (request, response) {
     // Give a simple login form
@@ -56,34 +48,34 @@ app.get("/login", function (request, response) {
     `;
     //takes html and barfs it back
     response.send(str);
- });
+});
+
+/*app.get("/invoice", function (request, response) {
+    
+    response.send(JSON.stringify(users_product_quantities));
+});*/
 
 app.post("/login", function (request, response) {
+    console.log(request.body);
+    the_username = request.body.username;
 
-console.log(request.body);
-
-the_username = request.body.username;
-
-if(typeof users_reg_data[the_username] != 'undefined'){
+    if(typeof users_reg_data[the_username] != 'undefined'){
     //checking to see if username exists
-    if (users_reg_data[the_username].password == request.body.password){
-        //if it does exists, get the password
-        //send them to invoice
-        response.send(the_username + 'logged in');
-    }   //login
-        //response when the user is logged in to greet them 
-        //personalization
-    else {response.redirect('/login');
-        //redirects the user to the login page
-}
-    
-
-}
+        if (users_reg_data[the_username].password == request.body.password){
+        //make the QS of product quantities needed for invoice
+        theQuantityQuerystring = qs.stringify(user_product_quantities);
+        response.redirect('/invoice.html?' + theQuantityQuerystring);
+        //go to invoice.html
+    }else {
+        response.redirect('/login');
+        }
+    }
 });
+
 app.get("/register", function (request, response) {
     // Give a simple register form
     str = `
-<body>
+    <body>
     <form action="" method="POST">
         <input type="text" name="username" size="40" placeholder="enter username" ><br />
         <input type="password" name="password" size="40" placeholder="enter password"><br />
@@ -91,12 +83,12 @@ app.get("/register", function (request, response) {
         <input type="email" name="email" size="40" placeholder="enter email"><br />
         <input type="submit" value="Submit" id="submit">
     </form>
-</body>
+    </body>
     `;
     response.send(str);
- });
+});
 
- app.post("/register", function (request, response) {
+app.post("/register", function (request, response) {
     // process a simple register form
 
     //validate registration data()
@@ -110,21 +102,8 @@ app.get("/register", function (request, response) {
     fs.writeFileSync(filename, JSON.stringify(users_reg_data));
 
     response.send(`${username} registered!`)
- });
+});
 
+app.use(express.static('./public'));
+//order matters
 app.listen(8080, () => console.log(`listening on port 8080`));
-
-//put into if statement
-// if console.log(typeof users_reg_data['xxx']); /= undefined
-//console.log(typeof users_reg_data['xxx']); 
-//check to see if it exists 'undefined' if exists 'object'
-
-//login and register are forms
-//querystring what you see at end of url after ?
-//server getting data in a query string
-
-//login give QS to registration, then reg keeps it 
-//always add QS to every server request you make
-
-//3 lines of code??
-//
